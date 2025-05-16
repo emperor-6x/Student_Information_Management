4d9bf4952af811fe1ffc4561d435b12b438e3bd5
Emperor <166212416+emperor-6x@users.noreply.github.com>
2025-05-16 11:52:21 +0530

    StudentService.java

--- StudentService.java
+++ StudentService.java
@@ -13,12 +13,27 @@ public class StudentService {
         this.studentDAO = studentDAO;
     }
 
+    public void addStudent(Student student) throws SQLException {
+        studentDAO.addStudent(student);
+    }
+
     public List<Student> getAllStudents() throws SQLException {
         return studentDAO.getAllStudents();
     }
 
-    public void addStudent(Student student) throws SQLException {
-        // Add Validations Here if Necessary
-        studentDAO.addStudent(student);
+    public void deleteStudent(int studentId) throws SQLException {
+        studentDAO.deleteStudent(studentId);
+    }
+
+    public Student getStudentById(int studentId) throws Exception {
+        // Delegate the retrieval logic to the DAO layer
+        Student student = studentDAO.findStudentById(studentId);
+        if (student == null) {
+            throw new Exception("Student with ID " + studentId + " not found.");
+        }
+        return student;
+    }
+
+    public void updateStudent(Student student) {
     }
 }
